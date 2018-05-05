8aedcb72e2da0b8f5250d359a981a4f09aca0b4d
Lev <atarapugin@gmail.com>
2018-05-05 19:42:26 +0300

    Updating StackList and Stack component + created Card component

--- flashcardpro/src/components/Stack.js
+++ flashcardpro/src/components/Stack.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Card from './Card';
 
-export default class Stack extends Component {
+class Stack extends Component {
 	render() {
+		const { title, cards } = this.props.stack;
+
 		return (
 			<div>
 				<Link to='/'>Home</Link>
-				<h3>example title</h3>
+				<h3>{title}</h3>
+				<br/>
+				{
+					cards.map(card => {
+						return (
+							<Card key={card.id} card={card}/>
+						)
+					})
+				}
 			</div>
 		)
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		stack: state
+	}
+}
+
+export default connect(mapStateToProps)(Stack);
--- flashcardpro/src/components/StackList.js
+++ flashcardpro/src/components/StackList.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import stacks from '../data/stacks.json';
 import { Link } from 'react-router-dom';
+import { setStack } from "../actions";
 
 
-export default class StackList extends Component {
+class StackList extends Component {
 	render() {
+
+		const { setStack } = this.props;
+
 		return (
 			<div>
 				{
 					stacks.map(stack => {
 						return (
-							<Link to='stack' key={stack.id}>
+							<Link
+								to='stack'
+								key={stack.id}
+								onClick={() => setStack(stack)}
+							>
 								<h4>{stack.title}</h4>
 							</Link>
 						);
@@ -20,3 +29,5 @@ export default class StackList extends Component {
 		)
 	}
 }
+
+export default connect(null, { setStack })(StackList);
