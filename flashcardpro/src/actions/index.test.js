95b2afdf850a2570cdd178e51081afe3567bb7ff
Lev <atarapugin@gmail.com>
2018-05-08 09:39:55 +0300

    Tests for actions by flashcardpro

---
+++ flashcardpro/src/actions/index.test.js
@@ -0,0 +1,32 @@
+import * as actions from './index';
+import { stack, stacks } from '../data/fixtures';
+
+describe('actions', () => {
+
+	it('creates an action to set the main stack', () => {
+		const expectedAction = {
+			type: actions.SET_STACK,
+			payload: stack
+		};
+
+		expect(actions.setStack(stack)).toEqual(expectedAction);
+	});
+
+	it('creates an action to add a stack', () => {
+		const expectedAction = {
+			type: actions.ADD_STACK,
+			payload: stack
+		};
+
+		expect(actions.addStack(stack)).toEqual(expectedAction);
+	});
+
+	it('creates an action to load stacks', () => {
+		const expectedAction = {
+			type: actions.LOAD_STACKS,
+			payload: stacks
+		};
+
+		expect(actions.loadStacks(stacks)).toEqual(expectedAction);
+	});
+});
